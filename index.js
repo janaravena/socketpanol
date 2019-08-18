613352b4960651dbfc720f6ea225767001da6fba
Alejandra Aravena <janaravena@gmail.com>
2019-08-18 18:27:44 -0400

    actualizacion

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 let app = require('express')();
-let http = require('http').Server(app);
-let io = require('socket.io')(http);
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
  
 io.on('connection', (socket) => {
-  
+ 
   socket.on('disconnect', function(){
-    io.emit('users-changed', {user: socket.nickname, event: 'left'});   
+    io.emit('users-changed', {user: socket.username, event: 'left'});   
   });
  
-  socket.on('set-nickname', (nickname) => {
-    socket.nickname = nickname;
-    io.emit('users-changed', {user: nickname, event: 'joined'});    
+  socket.on('set-name', (name) => {
+    socket.username = name;
+    io.emit('users-changed', {user: name, event: 'joined'});    
   });
   
-  socket.on('add-message', (message) => {
-    io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
+  socket.on('send-message', (message) => {
+    io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date()});    
   });
 });
  
 var port = process.env.PORT || 3001;
  
-http.listen(port, function(){
+server.listen(port, function(){
    console.log('listening in http://localhost:' + port);
 });
